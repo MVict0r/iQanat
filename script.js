4222f6a595ab107e34cad0dd30bd886b6f6e0d97
Victor <vmenzhik132f@gmail.com>
2025-12-11 17:05:34 +0500

    commit

--- script.js
+++ script.js
@@ -20,40 +20,101 @@ document.addEventListener('click', (e) => {
 });
 
 
+// gsap.registerPlugin(ScrollTrigger);
+//
+// gsap.utils.toArray(".fade-item").forEach((elem) => {
+//
+//     gsap.fromTo(
+//       elem,
+//       { opacity: 0, y: 80 },
+//       {
+//           opacity: 1,
+//           y: 0,
+//           scrollTrigger: {
+//               trigger: elem,
+//               start: "top 85%",
+//               end: "top 50%",
+//               scrub: true
+//           }
+//       }
+//     );
+//
+//     gsap.to(elem, {
+//         opacity: 0,
+//         y: -80,
+//         scrollTrigger: {
+//             trigger: elem,
+//             start: "top 50%",
+//             end: "top 15%",
+//             scrub: true
+//         }
+//     });
+//
+// });
+
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.utils.toArray(".fade-item").forEach((elem) => {
 
-    gsap.fromTo(elem,
-        {
-            opacity: 0,
-            y: 80
-        },
-        {
-            opacity: 1,
-            y: 0,
-            duration: 1.5,
-            ease: "power2.out",
-            scrollTrigger: {
-                trigger: elem,
-                start: "top 80%",      // когда элемент появляется
-                end: "top 20%",        // когда он должен исчезнуть
-                scrub: true,           // плавность
-            }
-        }
+    gsap.fromTo(
+      elem,
+      { opacity: 0, y: 100 },
+      {
+          opacity: 1,
+          y: 0,
+          scrollTrigger: {
+              trigger: elem,
+              start: "top 90%",
+              end: "top 40%",
+              scrub: true
+          }
+      }
     );
 
-    // Исчезновение
-    gsap.to(elem, {
-        opacity: 0,
-        y: -80,
-        ease: "power2.out",
-        scrollTrigger: {
-            trigger: elem,
-            start: "top 20%",
-            end: "top -10%",
-            scrub: true,
-        }
-    });
+    gsap.fromTo(
+      elem,
+      { opacity: 1, y: 0 },
+      {
+          opacity: 0,
+          y: -100,
+          scrollTrigger: {
+              trigger: elem,
+              start: "top 40%",
+              end: "top 0%",
+              scrub: true
+          }
+      }
+    );
 
 });
+
+// gsap.utils.toArray(".fade-item").forEach((elem) => {
+//
+//     gsap.to(elem, {
+//         opacity: 1,
+//         y: 0,
+//         ease: "none",
+//         scrollTrigger: {
+//             trigger: elem,
+//             start: "top 90%",
+//             end: "top 10%",
+//             scrub: true
+//         }
+//     });
+//
+// });
+// gsap.utils.toArray(".fade-item").forEach((elem) => {
+//
+//     gsap.to(elem, {
+//         opacity: 0,
+//         y: -80,
+//         ease: "none",
+//         scrollTrigger: {
+//             trigger: elem,
+//             start: "top 30%",
+//             end: "top -20%",
+//             scrub: true
+//         }
+//     });
+//
+// });
