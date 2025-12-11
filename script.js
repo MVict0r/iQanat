b55c819eaaaefa6b072888bc8e8ef6a637dc54aa
Victor Menzhulin <98138575+MVict0r@users.noreply.github.com>
2025-12-11 16:28:15 +0500

    commit

--- script.js
+++ script.js
@@ -18,3 +18,42 @@ document.addEventListener('click', (e) => {
         langSelect.classList.remove('open');
     }
 });
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".fade-item").forEach((elem) => {
+
+    gsap.fromTo(elem,
+        {
+            opacity: 0,
+            y: 80
+        },
+        {
+            opacity: 1,
+            y: 0,
+            duration: 1.5,
+            ease: "power2.out",
+            scrollTrigger: {
+                trigger: elem,
+                start: "top 80%",      // когда элемент появляется
+                end: "top 20%",        // когда он должен исчезнуть
+                scrub: true,           // плавность
+            }
+        }
+    );
+
+    // Исчезновение
+    gsap.to(elem, {
+        opacity: 0,
+        y: -80,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: elem,
+            start: "top 20%",
+            end: "top -10%",
+            scrub: true,
+        }
+    });
+
+});
